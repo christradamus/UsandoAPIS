b3af7a660c15cccfbe0f16d2aa312ebb440e6719
christradamus <christianperezflores@gmail.com>
2022-07-18 17:19:47 -0400

    Initial Commit

---
+++ testYoutube/HeroStats.swift
@@ -0,0 +1,18 @@
+//
+//  HeroStats.swift
+//  testYoutube
+//
+//  Created by Valeria Muñoz toro on 18-07-22.
+//
+
+import Foundation
+
+struct HeroStats: Decodable{
+    let localized_name: String
+    let primary_attr: String
+    let attack_type: String
+    let legs: Int
+    let img: String
+    
+}
+
